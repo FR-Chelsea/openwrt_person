4c63e1e3efe1fd6f1da5c68382f9f9b18a86f621
FR-Chelsea <35002212+FR-Chelsea@users.noreply.github.com>
2020-03-12 17:27:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/Lienol/openwrt-package package/Lienol
-git clone https://github.com/coolsnowwolf/lede package/coolsnowwolf
+#git clone https://github.com/coolsnowwolf/lede package/coolsnowwolf
 git clone https://github.com/vernesong/OpenClash  package/OpenClash
 git clone https://github.com/ntlf9t/luci-app-dnspod  package/dnspod
 #git clone https://github.com/koolshare/ledesoft  package/ledesoft
