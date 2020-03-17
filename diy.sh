f2131e1370c3ab437587ec05ff0710c6d2d27d1f
FR-Chelsea <35002212+FR-Chelsea@users.noreply.github.com>
2020-03-18 01:14:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.88.1/192.168.88.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/Lienol/openwrt-package package/Lienol
 git clone https://github.com/coolsnowwolf/lede package/coolsnowwolf
